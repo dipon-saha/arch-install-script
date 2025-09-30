eeabc28eaf09e73e9b87d1a3df34a6bb9da2f1a1
Dipon Saha <85273153+dipon-saha@users.noreply.github.com>
2025-09-30 14:51:36 +0800

    Revert "up"
    This reverts commit 6c0dda30d3cbc5d784ce24e822c16c12e4b1e35e.

--- post_install.sh
+++ post_install.sh
@@ -52,6 +52,7 @@ echo "Enabling grub-btrfsd service..."
 sudo systemctl enable grub-btrfsd.service
 echo "Snapper installation and configuration completed."
 
+
 # Install Paru AUR helper
 echo "Installing Paru AUR helper..."
 git clone https://aur.archlinux.org/paru.git /tmp/paru
